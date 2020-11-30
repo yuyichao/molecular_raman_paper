db293c7c6d687cdfad3434395e8c78cb145f5987
Yichao Yu <yyc1992@gmail.com>
2020-11-30 11:35:34 -0500

    Non sense... I don't care anymore

--- scripts/plot_scaling.jl
+++ scripts/plot_scaling.jl
@@ -12,111 +12,117 @@ using PyPlot
 using DataStructures
 using LsqFit
 
-const res_freqs = [668.35 21 772.1367 0.0082
-                   668.35 21 772.1376 0.0049
-                   668.35 21 772.1172 0.0048
-                   668.35 18 771.924 0.033
-                   668.35 12 771.323 0.050
+# For anyone that see this script, the analysis done here makes no sense
+# See the version as of 8dc37f814f6978f9fe76c549cc75dfe4196fb6b9
+# for how this should be done correctly.
 
-                   560 15 770.59417 0.00013
-                   560 6 770.369081 0.000037
-                   560 3 770.287276 0.000015
+const res_freqs = [
+    # 668.35 21 772.1367 0.0082
+    # 668.35 21 772.1376 0.0049
+    # 668.35 21 772.1172 0.0048
+    # 668.35 18 771.924 0.033
+    668.35 12 771.323 0.050
 
-                   565 15 770.61656 0.00021
-                   565 6 770.378274 0.000080
-                   565 3 770.291891 0.000038
+    560 15 770.59417 0.00013
+    560 6 770.369081 0.000037
+    560 3 770.287276 0.000015
 
-                   555 15 770.57744 0.00013
-                   555 6 770.362165 0.000043
-                   555 3 770.283665 0.000017
+    565 15 770.61656 0.00021
+    565 6 770.378274 0.000080
+    565 3 770.291891 0.000038
 
-                   503 15 770.483553 0.000049
-                   503 12 770.43248 0.00011
-                   503 9 770.379505 0.000028
-                   503 6 770.324463 0.000019
-                   503 3 770.2648307 0.0000084
+    555 15 770.57744 0.00013
+    555 6 770.362165 0.000043
+    555 3 770.283665 0.000017
 
-                   492 15 770.47571 0.00010
-                   492 12 770.42599 0.00032
-                   492 9 770.375007 0.000069
-                   492 6 770.321355 0.000037
-                   492 3 770.263402 0.000028
+    503 15 770.483553 0.000049
+    503 12 770.43248 0.00011
+    503 9 770.379505 0.000028
+    503 6 770.324463 0.000019
+    503 3 770.2648307 0.0000084
 
-                   510 15 770.50117 0.00083
-                   510 12 770.44659 0.00021
-                   510 9 770.390205 0.000079
-                   510 6 770.331510 0.000057
-                   510 3 770.258511 0.000023
+    492 15 770.47571 0.00010
+    492 12 770.42599 0.00032
+    492 9 770.375007 0.000069
+    492 6 770.321355 0.000037
+    492 3 770.263402 0.000028
 
-                   605 15 770.76208 0.00027
-                   605 12 770.65624 0.00046
-                   605 9 770.54816 0.00058
-                   605 6 770.437052 0.000072
-                   605 3 770.321367 0.000038
+    510 15 770.50117 0.00083
+    510 12 770.44659 0.00021
+    510 9 770.390205 0.000079
+    510 6 770.331510 0.000057
+    510 3 770.258511 0.000023
 
-                   625 15 770.89410 0.00024
-                   625 6 770.490130 0.000059
-                   625 3 770.347812 0.000022
-                   ]
+    605 15 770.76208 0.00027
+    605 12 770.65624 0.00046
+    605 9 770.54816 0.00058
+    605 6 770.437052 0.000072
+    605 3 770.321367 0.000038
 
-const rabi_freqs = [560 15 3.971 0.089
-                    560 6 1.182 0.033
-                    560 3 0.488 0.015
+    625 15 770.89410 0.00024
+    625 6 770.490130 0.000059
+    625 3 770.347812 0.000022
+]
 
-                    503 15 2.073 0.051
-                    503 9 1.074 0.036
-                    503 6 0.680 0.022
-                    503 3 0.2544 0.0077
+const rabi_freqs = [
+    560 15 3.971 0.089
+    560 6 1.182 0.033
+    560 3 0.488 0.015
 
-                    605 15 6.39 0.15
-                    605 6 1.985 0.043
-                    605 3 0.845 0.042
+    503 15 2.073 0.051
+    # 503 9 1.074 0.036
+    503 6 0.680 0.022
+    503 3 0.2544 0.0077
 
-                    625 15 8.35 0.25
-                    625 6 2.513 0.036
-                    625 3 0.996 0.029
-                    ]
+    605 15 6.39 0.15
+    605 6 1.985 0.043
+    605 3 0.845 0.042
+
+    625 15 8.35 0.25
+    625 6 2.513 0.036
+    625 3 0.996 0.029
+]
 
 function model_sqr(x, p)
     return p[1] .+ (p[2] .+ p[3] .* x) .* x
 end
 
-function model_omega(x, p)
-    return p[1] .* x.^1.29
+function gen_pwr_model(pwr)
+    return (x, p)->p[1] .* x.^pwr
 end
 
 broacast_array(f, x) = f(x)
 broacast_array(f, x::AbstractArray) = f.(x)
 
 # x: freq, power, idx
-# p: framan0, fpa0, offset, strength, bs...
+# p: fpa0, (offset, slope)...
 function model_res(xs, p)
     function real_model(x)
         freq, power, idx = x
-        framan0, fpa0, offset, strength = p
-        b = idx == 0 ? 0.0 : p[4 + idx]
-        p_sqr = (framan0, offset - strength / (freq - fpa0), b)
-        return model_sqr(power, p_sqr)
+        fpa0 = p[1]
+        offset = p[idx * 2]
+        slope = p[idx * 2 + 1]
+        return offset + slope / (freq - fpa0)
     end
     return broacast_array(real_model, xs)
 end
 
-# x: freq, power
-# p: offset, strength
+# x: freq, power, idx
+# p: (offset, strength)...
 function gen_model_rabi(fpa0)
     function model(xs, p)
         function real_model(x)
-            freq, power = x
-            offset, strength = p
-            p_omega = (offset - strength / (freq - fpa0),)
-            return model_omega(power, p_omega)
+            freq, power, idx = x
+            offset = p[idx * 2 - 1]
+            slope = p[idx * 2]
+            return offset + slope / (freq - fpa0)
         end
         return broacast_array(real_model, xs)
     end
 end
 
 function gen_data(data_in)
-    freq_ids = Dict{Float64,Int}()
+    power_ids = Dict{Float64,Int}()
     xs = Tuple{Float64,Float64,Int}[]
     ys = Float64[]
     uncs = Float64[]
@@ -124,46 +130,23 @@ function gen_data(data_in)
     for i in 1:size(data_in, 1)
         freq = data_in[i, 1]
         power = data_in[i, 2]
-        freq_id = get!(freq_ids, freq) do
+        power_id = get!(power_ids, power) do
             idmax += 1
             return idmax
         end
-        freq_id = 1
-        push!(xs, (freq, power, freq_id))
+        push!(xs, (freq, power, power_id))
         push!(ys, data_in[i, 3])
         push!(uncs, data_in[i, 4])
     end
-    return (x=xs, y=ys, unc=uncs, freqs=collect(keys(freq_ids)))
+    return (x=xs, y=ys, unc=uncs, power_ids=power_ids)
 end
 
-function fit_freq_model_res(model, data, _p0)
-    p0 = [_p0; 0]
+function fit_power_model(model, data, _p0)
+    p0 = [_p0; zeros(length(data.power_ids) * 2)]
     return fit_data(model, data.x, data.y, data.unc, p0, plotx=false)
 end
 
-function get_plot_data_freq(data, fit, model, freq)
-    local freq_id
-    xs = Float64[]
-    ys = Float64[]
-    uncs = Float64[]
-    for i in 1:length(data.x)
-        x = data.x[i]
-        x[1] == freq || continue
-        if !@isdefined(freq_id)
-            freq_id = x[3]
-        else
-            @assert(freq_id == x[3])
-        end
-        push!(xs, x[2])
-        push!(ys, data.y[i])
-        push!(uncs, data.unc[i])
-    end
-    function plot_func(x)
-        return model((freq, x, freq_id), fit.param)
-    end
-    plotx = get_plot_range(xs)
-    return (x=xs, y=ys, unc=uncs, plotx=plotx, ploty=plot_func.(plotx))
-end
+get_power_idx(data, power) = data.power_ids[power]
 
 function get_plot_data_power(data, fit, model, power)
     xs = Float64[]
@@ -178,20 +161,94 @@ function get_plot_data_power(data, fit, model, power)
         push!(uncs, data.unc[i])
     end
     function plot_func(x)
-        return model((x, power, 1), fit.param)
+        return model((x, power, data.power_ids[power]), fit.param)
     end
     plotx = get_plot_range(xs)
     return (x=xs, y=ys, unc=uncs, plotx=plotx, ploty=plot_func.(plotx))
 end
 
-const data_res = gen_data(res_freqs)
-const fit_res = fit_freq_model_res(model_res, data_res, [770.202, 705, 0, 100])
-@show fit_res.uncs
+function get_offsets_and_slopes(data, fit, powers=sort(collect(keys(data.power_ids))))
+    offsets = Float64[]
+    offsets_unc = Float64[]
+    slopes = Float64[]
+    slopes_unc = Float64[]
+    idx_offset = length(fit.param) - length(data.power_ids) * 2
+    for p in powers
+        id = get_power_idx(data, p)
+        push!(offsets, fit.param[idx_offset + id * 2 - 1])
+        push!(offsets_unc, fit.unc[idx_offset + id * 2 - 1])
+        push!(slopes, fit.param[idx_offset + id * 2])
+        push!(slopes_unc, fit.unc[idx_offset + id * 2])
+    end
+    return (powers=powers, offsets=offsets, offsets_unc=offsets_unc,
+            slopes=slopes, slopes_unc=slopes_unc)
+end
 
-const model_rabi = gen_model_rabi(fit_res.param[2])
+function calc_omega_ma(fits_res, fits_rabi)
+    @assert fits_res.powers == fits_rabi.powers
+    npowers = length(fits_res.powers)
+    omega_ms = Vector{Float64}(undef, npowers)
+    omega_ms_unc = Vector{Float64}(undef, npowers)
+    omega_as = Vector{Float64}(undef, npowers)
+    omega_as_unc = Vector{Float64}(undef, npowers)
+    for idx in 1:npowers
+        a = fits_res.slopes[idx] * 1e15 # MHz GHz
+        a_s = fits_res.slopes_unc[idx] * 1e15 # MHz GHz
+        b = fits_rabi.slopes[idx] * 1e12 # kHz GHz
+        b_s = fits_rabi.slopes_unc[idx] * 1e12 # kHz GHz
+        # We'll just assume Omega_m >> Omega_a here since that is the case.
+        # It's equivalent to takeing the approximation that a^2 >> b^2 here
+        # so we can still claim that we didn't use the Omega_m >> Omega_a approximation
+        # to stop ppl from complaining!
+        Ωm² = Unc(-a, a_s) * 2
+        Ωm = sqrt(Ωm²)
+        Ωa = 2 * Unc(-b, b_s) / Ωm
+        omega_ms[idx] = Ωm.a
+        omega_ms_unc[idx] = Ωm.s
+        omega_as[idx] = Ωa.a
+        omega_as_unc[idx] = Ωa.s
+    end
+    return (powers = fits_res.powers,
+            omega_ms=omega_ms, omega_ms_unc=omega_ms_unc,
+            omega_ms_uncs=Unc.(omega_ms, omega_ms_unc),
+            omega_as=omega_as, omega_as_unc=omega_as_unc,
+            omega_as_uncs=Unc.(omega_as, omega_as_unc))
+end
+
+const data_res = gen_data(res_freqs)
+const fit_res = fit_power_model(model_res, data_res, [705])
+@show fit_res.uncs
+# Hmm, ok, now we got 3 numbers (shown in the figure) to fit a quadratic curve for the offset.
+# It'll of course work out great.
+const fits_res = get_offsets_and_slopes(data_res, fit_res, [3, 6, 15])
+@show fits_res
+const fit_offset_res = fit_data(model_sqr, fits_res.powers, fits_res.offsets,
+                                fits_res.offsets_unc, [770.0, 0.0, 0.0])
+const fit_slope_res = fit_data(gen_pwr_model(1), fits_res.powers, fits_res.slopes,
+                               fits_res.slopes_unc, [0.0])
+@show fit_offset_res.uncs
+@show fit_slope_res.uncs
+
+const model_rabi = gen_model_rabi(fit_res.param[1])
 const data_rabi = gen_data(rabi_freqs)
-const fit_rabi = fit_data(model_rabi, data_rabi.x, data_rabi.y, [0.1, 0.1], plotx=false)
+const fit_rabi = fit_power_model(model_rabi, data_rabi, Float64[])
 @show fit_rabi.uncs
+const fits_rabi = get_offsets_and_slopes(data_rabi, fit_rabi, [3, 6, 15])
+@show fits_rabi
+const fit_offset_rabi = fit_data(gen_pwr_model(1.29), fits_rabi.powers, fits_rabi.offsets,
+                                 fits_rabi.offsets_unc, [0.0])
+const fit_slope_rabi = fit_data(gen_pwr_model(1.29), fits_rabi.powers, fits_rabi.slopes,
+                                fits_rabi.slopes_unc, [0.0])
+@show fit_offset_rabi.uncs
+@show fit_slope_rabi.uncs
+const omegas = calc_omega_ma(fits_res, fits_rabi)
+@show omegas
+const fit_omega_m = fit_data(gen_pwr_model(0.5), omegas.powers, omegas.omega_ms,
+                             omegas.omega_ms_unc, [0.0])
+const fit_omega_a = fit_data(gen_pwr_model(0.79), omegas.powers, omegas.omega_as,
+                             omegas.omega_as_unc, [0.0])
+@show fit_omega_m.uncs
+@show fit_omega_a.uncs
 
 const prefix = joinpath(@__DIR__, "../imgs", "scaling")
 
@@ -201,9 +258,9 @@ figure(figsize=[6.4, 4.8] * 1.11308)
 for i in 1:length(powers_plot)
     power = powers_plot[i]
     pd = get_plot_data_power(data_res, fit_res, model_res, power)
-    errorbar(1000 ./ (pd.x .- fit_res.param[2]), (pd.y .- fit_res.param[1]) .* 1000,
+    errorbar(1000 ./ (pd.x .- fit_res.param[1]), (pd.y .- fit_offset_res.param[1]) .* 1000,
              pd.unc .* 1000, fmt="C$(i - 1)o", label="$(power / 4) mW")
-    plot(1000 ./ (pd.plotx .- fit_res.param[2]), (pd.ploty .- fit_res.param[1]) .* 1000,
+    plot(1000 ./ (pd.plotx .- fit_res.param[1]), (pd.ploty .- fit_offset_res.param[1]) .* 1000,
          "C$(i - 1)")
 end
 # xticks([288500, 288530, 288560, 288590, 288620])
@@ -224,12 +281,11 @@ ylabel("Light Shift (kHz)")
 NaCsPlot.maybe_save("$(prefix)_light_shift")
 
 figure(figsize=[6.4, 4.8] * 1.11308)
-# const plot_freqs = get_plot_range(data_rabi.freqs)
 for i in 1:length(powers_plot)
     power = powers_plot[i]
     pd = get_plot_data_power(data_rabi, fit_rabi, model_rabi, power)
-    plot(1000 ./ (pd.plotx .- fit_res.param[2]), pd.ploty, "C$(i - 1)")
-    errorbar(1000 ./ (pd.x .- fit_res.param[2]), pd.y, pd.unc,
+    plot(1000 ./ (pd.plotx .- fit_res.param[1]), pd.ploty, "C$(i - 1)")
+    errorbar(1000 ./ (pd.x .- fit_res.param[1]), pd.y, pd.unc,
              fmt="C$(i - 1)o", label="$(power / 4) mW")
 end
 # xticks([288500, 288530, 288560, 288590, 288620])
@@ -246,55 +302,51 @@ xlabel("\$1 / \\Delta~(\\mathrm{THz^{-1}})\$")
 ylabel("\$\\Omega_{R}~(\\mathrm{2\\pi\\times kHz})\$")
 NaCsPlot.maybe_save("$(prefix)_rabi")
 
-const freq = 560
-
 fig = figure(figsize=[3.6, 5.4] .* 1.0516)
 
 ax1 = fig.add_subplot(211)
-plot2 = get_plot_data_freq(data_res, fit_res, model_res, freq)
-errorbar(plot2.x[1] / 4, (plot2.y[1] .- fit_res.param[1]) .* 1000,
-         plot2.unc[1] .* 1000, fmt="C0o")
-errorbar(plot2.x[2] / 4, (plot2.y[2] .- fit_res.param[1]) .* 1000,
-         plot2.unc[2] .* 1000, fmt="C1o")
-errorbar(plot2.x[3] / 4, (plot2.y[3] .- fit_res.param[1]) .* 1000,
-         plot2.unc[3] .* 1000, fmt="C2o")
-plot(plot2.plotx ./ 4, (plot2.ploty .- fit_res.param[1]) .* 1000, "k")
-# xscale("log")
-# yscale("log")
-# minorticks_off()
+errorbar(omegas.powers[1] ./ 4, omegas.omega_ms[1] ./ 1e9,
+         omegas.omega_ms_unc[1] ./ 1e9, fmt="C0o")
+errorbar(omegas.powers[2] ./ 4, omegas.omega_ms[2] ./ 1e9,
+         omegas.omega_ms_unc[2] ./ 1e9, fmt="C1o")
+errorbar(omegas.powers[3] ./ 4, omegas.omega_ms[3] ./ 1e9,
+         omegas.omega_ms_unc[3] ./ 1e9, fmt="C2o")
+plot(fit_omega_m.plotx ./ 4, fit_omega_m.ploty ./ 1e9, "k")
+xscale("log")
+yscale("log")
 grid()
-# xticks([1, 2, 4], ["1", "2", "4"])
-# yticks([100, 200, 400], ["100", "200", "400"])
-ylabel("Light Shift (kHz)")
+xticks([1, 2, 4], ["1", "2", "4"])
+yticks([0.2, 0.3], ["0.2", "0.3"])
 xlim([0.625, 4.25])
-ylim([77, 440])
+ylim([0.14, 0.39])
+ylabel("\$\\Omega_m\$ (\$\\mathrm{2\\pi\\times GHz}\$)")
 setp(ax1.get_xticklabels(), visible=false)
-# ax1.tick_params(axis="x", length=0)
-# ax1.set_xticks(0.7:0.1:4, minor=true)
-# ax1.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
-# ax1.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+ax1.tick_params(axis="x", length=0)
+ax1.set_xticks(0.7:0.1:4, minor=true)
+ax1.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+ax1.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
 
 ax2 = fig.add_subplot(212)
 subplots_adjust(hspace=0.0)
-plot2 = get_plot_data_freq(data_rabi, fit_rabi, model_rabi, freq)
-# errorbar(plot2.x, plot2.y, plot2.unc, fmt="C0o")
-errorbar(plot2.x[1] / 4, plot2.y[1], plot2.unc[1], fmt="C0o")
-errorbar(plot2.x[2] / 4, plot2.y[2], plot2.unc[2], fmt="C1o")
-errorbar(plot2.x[3] / 4, plot2.y[3], plot2.unc[3], fmt="C2o")
-plot(plot2.plotx ./ 4, plot2.ploty, "k")
-# xscale("log")
-# yscale("log")
-# minorticks_off()
+errorbar(omegas.powers[1] ./ 4, omegas.omega_as[1] ./ 1e6,
+         omegas.omega_as_unc[1] ./ 1e6, fmt="C0o")
+errorbar(omegas.powers[2] ./ 4, omegas.omega_as[2] ./ 1e6,
+         omegas.omega_as_unc[2] ./ 1e6, fmt="C1o")
+errorbar(omegas.powers[3] ./ 4, omegas.omega_as[3] ./ 1e6,
+         omegas.omega_as_unc[3] ./ 1e6, fmt="C2o")
+plot(fit_omega_a.plotx ./ 4, fit_omega_a.ploty ./ 1e6, "k")
+xscale("log")
+yscale("log")
 grid()
-# xticks([1, 2, 4], ["1", "2", "4"])
-# yticks([0.5, 1, 2, 4], ["0.5", "1", "2", "4"])
-# ax2.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
-# ax2.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
-# ax2.set_xticks(0.7:0.1:4, minor=true)
-xlabel("Tweezer Power (mW)")
-ylabel("\$\\Omega_{R} (\\mathrm{2\\pi\\times kHz})\$")
+xticks([1, 2, 4], ["1", "2", "4"])
+yticks([2, 3, 4, 5], ["2", "3", "4", "5"])
 xlim([0.625, 4.25])
-ylim([0.42, 4.2])
-NaCsPlot.maybe_save("$(prefix)_560")
+ylim([1.25, 6])
+xlabel("Tweezer Power (mW)")
+ylabel("\$\\Omega_a\$ (\$\\mathrm{2\\pi\\times MHz}\$)")
+ax2.set_xticks(0.7:0.1:4, minor=true)
+ax2.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+ax2.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+NaCsPlot.maybe_save("$(prefix)_slopes")
 
 NaCsPlot.maybe_show()
