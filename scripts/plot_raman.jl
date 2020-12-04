2307b2a06e3fec2035f8da8cd62f73fd2ae1f99f
Yichao Yu <yyc1992@gmail.com>
2020-12-03 22:55:11 -0500

    Update figures
    * consistent 2pi
    * remove theory plot for absolute Omega_m and Omega_a

--- scripts/plot_raman.jl
+++ scripts/plot_raman.jl
@@ -25,44 +25,46 @@ const data = LibArchive.Reader(fname) do reader
 end
 const prefix = joinpath(@__DIR__, "../imgs", "raman")
 
-fig = figure(figsize=[1.11, 1.5] * 4.8)
+# fig = figure(figsize=[1.11, 1.5] * 4.8)
+fig = figure()
 
-ax1 = fig.add_subplot(311)
-plot(data[:, 1] .- 339724.57, abs.(data[:, 2] ./ 2π / 1000), "C1--",
-     label="\$v'=40\$")
-plot(data[:, 1] .- 288625.081, abs.(data[:, 2] ./ 2π / 1000), "C0",
-     label="\$v'=0\$")
-ylabel("\$\\Omega_{R}~(2\\pi\\!\\times\\!\\mathrm{kHz})\$")
-xlim([-300, 300])
-ylim([6, 20000])
-yscale("log")
-# yticks([10, 100, 1000, 10000], ["10", "100", "1000", "10000"])
-ax1.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
-grid()
-setp(ax1.get_xticklabels(), visible=false)
-setp(ax1.get_xticklabels(), visible=false)
-ax1.tick_params(axis="x", length=0)
+# ax1 = fig.add_subplot(311)
+# plot(data[:, 1] .- 339724.57, abs.(data[:, 2] ./ 2π / 1000), "C1--",
+#      label="\$v'=40\$")
+# plot(data[:, 1] .- 288625.081, abs.(data[:, 2] ./ 2π / 1000), "C0",
+#      label="\$v'=0\$")
+# ylabel("\$\\Omega_{R}~(2\\pi\\!\\times\\!\\mathrm{kHz})\$")
+# xlim([-300, 300])
+# ylim([6, 20000])
+# yscale("log")
+# # yticks([10, 100, 1000, 10000], ["10", "100", "1000", "10000"])
+# ax1.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+# grid()
+# setp(ax1.get_xticklabels(), visible=false)
+# setp(ax1.get_xticklabels(), visible=false)
+# ax1.tick_params(axis="x", length=0)
 
-ax2 = fig.add_subplot(312)
-subplots_adjust(hspace=0.018)
-plot(data[:, 1] .- 339724.57, abs.(data[:, 4] ./ 2π / 1000), "C1--",
-     label="\$v'=40\$")
-plot(data[:, 1] .- 288625.081, abs.(data[:, 4] ./ 2π / 1000), "C0",
-     label="\$v'=0\$")
-ylabel("\$\\Gamma_{s}~(2\\pi\\!\\times\\!\\mathrm{kHz})\$")
-legend(fontsize=13.88, loc="upper left", handlelength=1, handletextpad=0.3)
-xlim([-300, 300])
-ylim([0.12, 200000])
-yscale("log")
-# yticks([1, 10, 100, 1000, 10000], ["1", "10", "100", "1000", "10000"])
-ax2.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
-grid()
-setp(ax2.get_xticklabels(), visible=false)
-setp(ax2.get_xticklabels(), visible=false)
-ax2.tick_params(axis="x", length=0)
+# ax2 = fig.add_subplot(312)
+# subplots_adjust(hspace=0.018)
+# plot(data[:, 1] .- 339724.57, abs.(data[:, 4] ./ 2π / 1000), "C1--",
+#      label="\$v'=40\$")
+# plot(data[:, 1] .- 288625.081, abs.(data[:, 4] ./ 2π / 1000), "C0",
+#      label="\$v'=0\$")
+# ylabel("\$\\Gamma_{s}~(2\\pi\\!\\times\\!\\mathrm{kHz})\$")
+# legend(fontsize=13.88, loc="upper left", handlelength=1, handletextpad=0.3)
+# xlim([-300, 300])
+# ylim([0.12, 200000])
+# yscale("log")
+# # yticks([1, 10, 100, 1000, 10000], ["1", "10", "100", "1000", "10000"])
+# ax2.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+# grid()
+# setp(ax2.get_xticklabels(), visible=false)
+# setp(ax2.get_xticklabels(), visible=false)
+# ax2.tick_params(axis="x", length=0)
 
-ax3 = fig.add_subplot(313)
-subplots_adjust(hspace=0.018)
+# ax3 = fig.add_subplot(313)
+# subplots_adjust(hspace=0.018)
+ax3 = gca()
 plot(data[:, 1] .- 288625.081, abs.(data[:, 2] ./ data[:, 4]), "C0",
      label="\$v'=0\$")
 plot(data[:, 1] .- 339724.57, abs.(data[:, 2] ./ data[:, 4]), "C1--",
