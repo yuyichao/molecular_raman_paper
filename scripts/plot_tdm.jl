06acacea2607340b889eeb6d218df6d21d32ea22
Yichao Yu <yyc1992@gmail.com>
2021-06-29 09:36:22 -0400

    [imgs] TDM comparison

--- scripts/plot_tdm.jl
+++ scripts/plot_tdm.jl
@@ -33,6 +33,15 @@ const data_a3c3 = read_csv_compressed(fname_a3c3, ',', Float64)
 const data_X1A1 = read_csv_compressed(fname_X1A1, ',', Float64)
 const data_X1B1 = read_csv_compressed(fname_X1B1, ',', Float64)
 
+const fname_S3P3 = joinpath(@__DIR__, "../data/TDM_Dulieu/Sigma3_Pi3_1_1-7.csv.zst")
+const fname_S3S3 = joinpath(@__DIR__, "../data/TDM_Dulieu/Sigma3_Sigma3_1_1-7.csv.zst")
+const fname_S1S1 = joinpath(@__DIR__, "../data/TDM_Dulieu/Sigma1_Sigma1_1_1-7.csv.zst")
+const fname_S1P1 = joinpath(@__DIR__, "../data/TDM_Dulieu/Sigma1_Pi1_1_1-7.csv.zst")
+const data_S3P3 = read_csv_compressed(fname_S3P3, ',', Float64)
+const data_S3S3 = read_csv_compressed(fname_S3S3, ',', Float64)
+const data_S1S1 = read_csv_compressed(fname_S1S1, ',', Float64)
+const data_S1P1 = read_csv_compressed(fname_S1P1, ',', Float64)
+
 const prefix = joinpath(@__DIR__, "../imgs", "TDM")
 
 figure(figsize=[13.8, 4.8])
@@ -79,5 +88,31 @@ xlabel("R (Angstroms)")
 ylabel("Transition Dipole Moment (\$ea_0\$)")
 legend(fontsize=13.88)
 NaCsPlot.maybe_save("$(prefix)")
+
+figure(figsize=[6.4, 4.8])
+plot(data_a3b3[:, 1] .* 0.529177210903, data_a3b3[:, 2], "C1",
+     label="\$a^3\\Sigma\\rightarrow b^3\\Pi\$")
+plot(data_S3P3[:, 1] .* 0.529177210903, data_S3P3[:, 2], "C1--")
+plot(data_a3c3[:, 1] .* 0.529177210903, data_a3c3[:, 2], "C2",
+     label="\$a^3\\Sigma\\rightarrow c^3\\Sigma\$")
+plot(data_S3S3[:, 1] .* 0.529177210903, data_S3S3[:, 3], "C2--")
+plot(data_X1A1[:, 1] .* 0.529177210903, data_X1A1[:, 2], "C3",
+     label="\$X^1\\Sigma\\rightarrow A^1\\Sigma\$")
+plot(data_S1S1[:, 1] .* 0.529177210903, data_S1S1[:, 3], "C3--")
+plot(data_X1B1[:, 1] .* 0.529177210903, data_X1B1[:, 2], "C4",
+     label="\$X^1\\Sigma\\rightarrow B^1\\Pi\$")
+plot(data_S1P1[:, 1] .* 0.529177210903, data_S1P1[:, 2], "C4--")
+grid()
+xscale("log")
+xlim([2.7, 18])
+xticks([3, 4, 5, 6, 7, 8, 9, 10], ["3", "4", "5", "6", "7", "8", "9", "10"])
+# xticks([4, 6, 8, 10, 20], ["4", "6", "8", "10", "20"])
+# xticks([3, 10], ["3", "10"])
+gca().set_xticks([3:10], minor=true)
+gca().xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+xlabel("R (Angstroms)")
+ylabel("Transition Dipole Moment (\$ea_0\$)")
+legend(fontsize=13.88)
+NaCsPlot.maybe_save("$(prefix)_compare_dulieu")
 
 NaCsPlot.maybe_show()
